64d77883cd11304b75d7b778f60a565b1de7f338
Brandon Richardson <brandon.richardson@introhive.com>
2017-02-08 22:12:18 -0400

    Code Refactor
    Added comments, helper methods and improved cohesion. Improved performance.

--- src/SudokuGrid.java
+++ src/SudokuGrid.java
@@ -2,57 +2,132 @@ public class SudokuGrid
 {
     private GridCell[][] grid;
 
-    public SudokuGrid()
-    {
-    	this.grid = new GridCell[9][9];
-    	for(int i = 0; i < this.grid.length; i++)
-    	{
-    		for(int o = 0; o < this.grid[0].length; o++)
-    		{
-    			this.grid[i][o] = new GridCell();
-    		}
-    	}
-    }
-
+    /** SudokuGrid constructor. Initializes a 9x9 sudoku grid with values
+      * described by the parameter grid. The grid array must be 
+      * an array of dimensions 9x9, where empty cells have a value of 0.
+      * Empty cells are modifiable, and cells with values 1-9 will be
+      * marked as unmodifiable, and will not respond to incrementCell(),
+      * setCell() or resetCell().
+      * 
+      * Each cell is assigned an array of valid cell values based on the initial
+      * cell values. If the cell has only one possible value, the cell value is
+      * set to this value and is marked as unmodifiable. This will simplify
+      * the puzzle and decrease the amount of time needed to find a solution.
+      * This may have a domino effect, spawning other cells to have only a single
+      * valid cell value, which will simplify the puzzle even further.
+      * @param grid The initial sudoku cells given, used to construct a grid object
+      */
     public SudokuGrid(int[][] grid)
     {
-    	this.grid = new GridCell[9][9];
-    	for(int i = 0; i < this.grid.length; i++)
-    	{
-    		for(int o = 0; o < this.grid[0].length; o++)
-    		{
-    			this.grid[i][o] = new GridCell();
-    		}
-    	}
+    	this.buildEmptyGrid();
     	
+    	//Reject Invalid Parameter Array
     	if(grid.length != 9 || grid[0].length != 9)
-        	throw new RuntimeException("provided grid is invalid");
+        	throw new RuntimeException("provided grid has invalid dimensions");
     	
-        for(int i = 0; i < grid.length; i++)
+    	//Build SudokuGrid from Parameter Array
+        for(int row = 0; row < grid.length; row++)
     	{
-    		for(int o = 0; o < grid[0].length; o++)
+    		for(int col = 0; col < grid[0].length; col++)
     		{
-    			if(grid[i][o] == 0)
+    			if(grid[row][col] < 0 || grid[row][col] > 9)
+    				throw new RuntimeException("provided grid has invalid cell; value: " + grid[row][col]);
+    			
+    			if(grid[row][col] == 0)
     			{
-    				this.grid[i][o].modifiable = true;
+    				this.grid[row][col].modifiable = true;
     				continue;
     			}
     			else
     			{
-    				this.grid[i][o].value = grid[i][o];
-    				this.grid[i][o].modifiable = false;
+    				this.grid[row][col].value = grid[row][col];
+    				this.grid[row][col].modifiable = false;
     			}
+    		}
+    	}
+        
+        //Fill Possible Cell Values Array
+        for(int row = 0; row < grid.length; row++)
+        {
+        	for(int col = 0; col < grid[0].length; col++)
+        	{
+        		int quadrant = this.getQuadrant(col, row);
+        		int[] possibleCellValues = new int[0];
+        		
+        		for(int number = 1; number < 10; number++)
+        		{
+        			if(!this.isCellModifiable(col, row))
+        			{
+        				this.grid[row][col].validCellValues = null;
+        				break;
+        			}
+        			else if(this.rowContains(row, number) == 0 && this.colContains(col, number) == 0 && this.quadrantContains(quadrant, number) == 0)
+        			{
+        				int[] newArray = new int[possibleCellValues.length + 1];
+        				System.arraycopy(possibleCellValues, 0, newArray, 0, possibleCellValues.length);
+        				possibleCellValues = newArray;
+        				possibleCellValues[possibleCellValues.length - 1] = number;
+        			}
+        		}
+        		
+        		//Attempt to simplify grid
+        		if(possibleCellValues.length == 1)
+        		{
+        			this.grid[row][col].value = possibleCellValues[0];
+        			this.grid[row][col].validCellValues = null;
+    				this.grid[row][col].modifiable = false;
+    				row = -1;
+    				break;
+        		}
+        		else
+        		{
+        			this.grid[row][col].validCellValues = possibleCellValues;
+        		}
+        	}
+        }
+    }
+    
+    /** Build an empty sudoku grid. Each cell has initial value 0, and is modifiable.
+      */
+    private void buildEmptyGrid()
+    {
+    	this.grid = new GridCell[9][9];
+    	for(int i = 0; i < this.grid.length; i++)
+    	{
+    		for(int o = 0; o < this.grid[0].length; o++)
+    		{
+    			this.grid[i][o] = new GridCell();
+    			this.grid[i][o].value = 0;
+    			this.grid[i][o].modifiable = true;
+    			this.grid[i][o].validCellValues = null;
     		}
     	}
     }
 
-    public boolean isValidRow(int row)
+    /** Validate grid row. Validation is done in two steps, first with an initial
+      * non-deterministic step, followed by a deterministic step.
+      * 
+      * The non-deterministic step rejects any grid whose sum of cell values across a
+      * given row is not equal to 45, or one with an empty cell. This acts as a fast
+      * way to invalidate a row.
+      * 
+      * The deterministic step tests the frequency of values in a row. The row is valid
+      * iff the frequency of numbers across a row are exactly 1, and the only numbers 
+      * that appear are digits 1 through 9.
+      * 
+      * @param y row
+      * @return true if the row is valid, false otherwise.
+      */
+    public boolean isValidRow(int y)
     {
-        //If Sum of Indices != 45, Row is Invalid
+        //If Sum of Indices != 45 or Empty Cell Encountered, Row is Invalid
         int sum = 0;
         for(int i = 0; i < 9; i++)
         {
-            sum += this.grid[row][i].value;
+            if(this.grid[y][i].value == 0)
+            	return false;
+            
+        	sum += this.grid[y][i].value;
         }
 
         if(sum != 45)
@@ -65,7 +140,7 @@ public class SudokuGrid
 
             for(int o = 0; o < 9; o++)
             {
-                if(this.grid[row][o].value == i)
+                if(this.grid[y][o].value == i)
                     count++;
             }
 
@@ -76,13 +151,27 @@ public class SudokuGrid
         return true;
     }
 
-    public boolean isValidColumn(int col)
+    /** Validate grid column. Validation is done in two steps, first with an initial
+      * non-deterministic step, followed by a deterministic step.
+      * 
+      * The non-deterministic step rejects any grid whose sum of cell values across a
+      * given column is not equal to 45, or one with an empty cell. This acts as a fast
+      * way to invalidate a column.
+      * 
+      * The deterministic step tests the frequency of values in a column. The column is valid
+      * iff the frequency of numbers across a column are exactly 1, and the only numbers 
+      * that appear are digits 1 through 9.
+      * 
+      * @param x column
+      * @return true if the column is valid, false otherwise.
+      */
+    public boolean isValidColumn(int x)
     {
         //If Sum of Indices != 45, Col is Invalid
         int sum = 0;
         for(int i = 0; i < 9; i++)
         {
-            sum += this.grid[i][col].value;
+            sum += this.grid[i][x].value;
         }
 
         if(sum != 45)
@@ -95,7 +184,7 @@ public class SudokuGrid
 
             for(int o = 0; o < 9; o++)
             {
-                if(this.grid[o][col].value == i)
+                if(this.grid[o][x].value == i)
                     count++;
             }
 
@@ -106,6 +195,20 @@ public class SudokuGrid
         return true;
     }
 
+    /** Validate grid quadrant. Validation is done in two steps, first with an initial
+      * non-deterministic step, followed by a deterministic step.
+      * 
+      * The non-deterministic step rejects any grid whose sum of cell values in a
+      * given quadrant is not equal to 45, or one with an empty cell. This acts as a fast
+      * way to invalidate a quadrant.
+      * 
+      * The deterministic step tests the frequency of values in a quadrant. The quadrant is valid
+      * iff the frequency of numbers are exactly 1, and the only numbers 
+      * that appear are digits 1 through 9.
+      * 
+      * @param quadrant
+      * @return true if the quadrant is valid, false otherwise.
+      */
     public boolean isValidQuadrant(int quadrant)
     {
         int xOffset = 0;
@@ -161,6 +264,12 @@ public class SudokuGrid
         return true;
     }
 
+    /** Validate the entire grid. Validates each row, column and quadrant individually.
+      * @see isValidColumn(int x)
+      * @see isValidRow(int y)
+      * @see isValidQuadrant(int quadrant)
+      * @return true if the grid is valid, false otherwise.
+      */
     public boolean isValid()
     {
         //Test Rows
@@ -187,30 +296,45 @@ public class SudokuGrid
         return true;
     }
 
-    public int rowContains(int row, int num)
+    /** Test the frequency of a given number across a row.
+      * @param y row
+      * @param num
+      * @return the number of occurrences of num in row
+      */
+    public int rowContains(int y, int num)
     {
         int count = 0;
         for(int o = 0; o < 9; o++)
         {
-            if(this.grid[row][o].value == num)
+            if(this.grid[y][o].value == num)
                 count++;
         }
 
         return count;
     }
 
-    public int colContains(int col, int num)
+    /** Test the frequency of a given number across a column.
+     * @param x column
+     * @param num
+     * @return the number of occurrences of num in col
+     */
+    public int colContains(int x, int num)
     {
         int count = 0;
         for(int o = 0; o < 9; o++)
         {
-            if(this.grid[o][col].value == num)
+            if(this.grid[o][x].value == num)
                 count++;
         }
 
         return count;
     }
 
+    /** Test the frequency of a given number in a quadrant.
+     * @param quadrant
+     * @param num
+     * @return the number of occurances of num in quadrant
+     */
     public int quadrantContains(int quadrant, int num)
     {
         int xOffset = 0;
@@ -245,33 +369,121 @@ public class SudokuGrid
         return count;
     }
 
-    public int getCell(int row, int col)
+    /** Access the value of the grid cell at the given row and column.
+     * @param x column
+     * @param y row
+     * @return the cell value at row and column
+     */
+    public int getCell(int x, int y)
     {
-        return this.grid[row][col].value;
+        return this.grid[y][x].value;
     }
 
-    public void incrementCell(int row, int col)
+    /** Compute the quadrant number specified by the row and column parameters.
+     * @param x column
+     * @param y row
+     * @return the quadrant number specified by row and col
+     */
+    public int getQuadrant(int x, int y)
     {
-    	if(!this.grid[row][col].modifiable)
-    		 throw new RuntimeException("Cannot increment cell; user defined");
-    	else if(this.grid[row][col].value == 9)
+    	if(x >= 0 && x <= 2)
+    	{
+    		if(y >= 0 && y <= 2)
+    			return 0;
+    		else if(y >= 3 && y <= 5)
+    			return 3;
+    		else
+    			return 6;
+    	}
+    	else if(x >= 3 && x <= 5)
+    	{
+    		if(y >= 0 && y <= 2)
+    			return 1;
+    		else if(y >= 3 && y <= 5)
+    			return 4;
+    		else
+    			return 7;
+    	}
+    	else
+    	{
+    		if(y >= 0 && y <= 2)
+    			return 2;
+    		else if(y >= 3 && y <= 5)
+    			return 5;
+    		else
+    			return 8;
+    	}
+    }
+    
+    /** Increment the cell at given row and column.
+     * @throws RuntimeException if cell is not modifiable, or the cell value is 9.
+     * @param x column
+     * @param y row
+     */
+    public void incrementCell(int x, int y)
+    {
+    	if(!this.grid[y][x].modifiable)
+    		throw new RuntimeException("Cannot increment cell; user defined");
+    	else if(this.grid[y][x].value == 9)
             throw new RuntimeException("Cannot increment cell; maximum possible cell value");
         else
-            this.grid[row][col].value++;
+            this.grid[y][x].value++;
     }
 
-    public void resetCell(int row, int col)
+    /** Set the value of a given cell.
+      * @param x column
+      * @param y row
+      * @param value the desired cell value
+      * @throws RuntimeException if the cell is not modifiable or the value does not
+      * fall within the range 1-9 
+      * */
+    public void setCell(int x, int y, int value)
     {
-        if(!this.grid[row][col].modifiable)
+    	if(!this.grid[y][x].modifiable)
+    		throw new RuntimeException("Cannot set cell value to" + value + "; cell not modifiable");
+    	else if(value > 9 || value < 1)
+           throw new RuntimeException("Cannot set cell value to" + value + "; must be 1-9");
+       else
+           this.grid[y][x].value = value;
+    }
+    
+    /** Reset the cell to an empty cell with value 0.
+     * @throws RuntimeException if cell is not modifiable.
+     * @param x column
+     * @param y row
+     */
+    public void resetCell(int x, int y)
+    {
+        if(!this.grid[y][x].modifiable)
         	 throw new RuntimeException("Cannot reset cell; user defined");
-    	this.grid[row][col].value = 0;
+    	this.grid[y][x].value = 0;
     }
     
-    public boolean isCellModifiable(int row, int col)
+    /** Returns whether the cell is modifiable.
+     * @param x column
+     * @param y row
+     * @return whether the cell is modifiable.
+     */
+    public boolean isCellModifiable(int x, int y)
     {
-    	return this.grid[row][col].modifiable;
+    	return this.grid[y][x].modifiable;
     }
     
+    /** Access an array containing all the possible values for a given cell.
+      * Based only on on the initial grid unmodifiable cell values.
+      * @param x column
+      * @param y row
+      * @return an array of all possible values for that cell
+      * */
+    public int[] getPossibleCellValues(int x, int y)
+    {
+    	return this.grid[y][x].validCellValues;
+    }
+    
+    /** Build an array of integers that represents the sudoku grid at the moment of
+     * being invoked.
+     * @return an array of integers representing this sudoku grid.
+     */
     public int[][] getGrid()
     {
     	int[][] newGrid = new int[9][9];
@@ -287,9 +499,14 @@ public class SudokuGrid
     	return newGrid;
     }
     
+    /** A class representing a single cell in a sudoku grid.
+      * Each cell has a value, and can be either modifiable or
+      * not modifiable.
+      */
     private class GridCell
     {
     	public boolean modifiable;
     	public int value;
+    	public int[] validCellValues;
     }
 }
--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,85 +1,238 @@
 public class SudokuSolver
 {
-    public static void main(String[] args)
+	private int iterations;
+	private int[] xy;
+	private int[][] initialGrid;
+	private int[][] solvedGrid;
+
+	/** Default SudokuSolver constructor. Initializes objects fields
+	  * to default values.
+	  */
+    public SudokuSolver()
     {
-    	int[][] initGrid = new int[9][9];
-    	initGrid[0][0] = 6;
-    	initGrid[0][3] = 7;
-    	initGrid[0][4] = 2;
-    	initGrid[0][7] = 3;
-    	initGrid[0][8] = 1;
-    	initGrid[1][1] = 3;
-    	initGrid[1][2] = 5;
-    	initGrid[1][3] = 1;
-    	initGrid[1][4] = 6;
-    	initGrid[1][5] = 9;
-    	initGrid[1][6] = 8;
-    	initGrid[2][0] = 9;
-    	initGrid[2][4] = 8;
-    	initGrid[3][1] = 5;
-    	initGrid[3][2] = 6;
-    	initGrid[3][4] = 9;
-    	initGrid[4][2] = 1;
-    	initGrid[4][7] = 7;
-    	initGrid[5][0] = 3;
-    	initGrid[5][1] = 4;
-    	initGrid[5][3] = 5;
-    	initGrid[5][6] = 2;
-    	initGrid[6][2] = 8;
-    	initGrid[6][3] = 6;
-    	initGrid[6][5] = 3;
-    	initGrid[6][6] = 7;
-    	initGrid[6][7] = 5;
-    	initGrid[7][1] = 7;
-    	initGrid[7][4] = 5;
-    	initGrid[7][8] = 3;
-    	initGrid[8][7] = 8;
-    	
-    	SudokuSolver.print(initGrid);
-    	int[][] solvedGrid = (new SudokuSolver()).solve(initGrid);
-    	
-    	SudokuSolver.print(solvedGrid);
+    	this.iterations = 0;
+    	this.xy = new int[]{0,0};
+    	this.initialGrid = null;
+    	this.solvedGrid = null;
     }
-
-    public SudokuSolver() {}
-
-    public int[][] solve(int[][] initgrid)
+   
+    /** Constructs a new SudokuSolver given an initial sudoku grid.
+      * @param initGrid the initial sudoku grid, used to initialize the grid with
+      * given cell values 
+      */
+    public SudokuSolver(int[][] initGrid)
     {
-    	SudokuGrid grid = new SudokuGrid(initgrid);
-    	int[] xy = new int[]{0,0};
-    	
-    	this.stepForward(xy);
-    	
-    	this.stepBackward(xy);
-    	
-    	
-    	
-    	
-    	return grid.getGrid();
+    	this.iterations = 0;
+    	this.xy = new int[]{0,0};
+    	this.initialGrid = initGrid;
+    	this.solvedGrid = null;
     }
     
-    private int[] stepForward(int[] xy)
+    /** Resets this object back to defaults. Clears all instance variables
+      */
+    public void reset()
     {
-    	if(xy[0] >= 8 && xy[1] >= 8)
-    		return xy;
+    	this.iterations = 0;
+    	this.xy = new int[]{0,0};
+    	this.initialGrid = null;
+    	this.solvedGrid = null;
+    }
+    
+    /** Resets the grid back to defaults, and initializes the new puzzle grid.
+      * @param initGrid the initial sudoku grid, used to initialize the grid with
+      * given cell values 
+      */
+    public void reset(int[][] initGrid)
+    {
+    	this.iterations = 0;
+    	this.xy = new int[]{0,0};
+    	this.initialGrid = initGrid;
+    	this.solvedGrid = null;
+    }
+    
+    /** Solves a given sudoku grid.
+      * @param initGrid the sudoku grid to solve
+      * @return the solved sudoku grid
+      */
+    public int[][] solve(int[][] initGrid)
+    {
+    	this.initialGrid = initGrid;
+    	return this.solve();
+    }
+    
+    /** Solve the sudoku grid represented by this SudokuSolver object.
+      * @return the solved sudoku grid
+      */
+    public int[][] solve()
+    {
+    	//Throw exception if the sudoku grid given is invalid
+    	if(this.initialGrid == null)
+    		throw new RuntimeException("Initial Grid Null. Initialize SudokuSolver with Initial Grid.");
+    	else if(this.initialGrid.length != 9 || this.initialGrid[0].length != 9)
+    		throw new RuntimeException("Invalid Grid Dimensions.");
     	
-    	if(xy[0] == 8)
+    	//Build SudokuGrid Object
+    	//Throw exception if the provided initial grid is invalid
+    	SudokuGrid grid = new SudokuGrid(this.initialGrid);
+    	for(int i = 0; i < 9; i++)
     	{
-    		xy[0] = 0;
-    		xy[1]++;
+    		for(int o = 1; o < 10; o++)
+    		{
+    			if(grid.rowContains(i, o) > 1 || grid.colContains(i, o) > 1 || grid.quadrantContains(i, o) > 1)
+    			{
+    				throw new RuntimeException("invalid sudoku puzzle; cannot be solved");
+    			}
+    		}
+    	}
+    	
+    	boolean backtrack = false;
+    	outerloop:
+    	while(true)
+    	{
+    		//Increment running total of iterations
+			iterations++;
+    		
+    		//If cell is not modifiable, skip
+    		if(!grid.isCellModifiable(this.xy[0], this.xy[1]))
+    		{
+    			if(!backtrack)
+    			{
+    				this.stepForward();
+    			}
+    			else
+    			{
+    				this.stepBackward();
+    			}
+    			
+    			continue;
+    		}
+    		
+    		//Get possible cell values for current cell
+    		backtrack = false;
+    		int[] possibleCellValues = grid.getPossibleCellValues(xy[0], xy[1]);
+    		
+    		//Iterate possible cell values
+    		int index = 0;
+    		for(int possibleValue : possibleCellValues)
+    		{
+    			//Catch up to cell value in possibleCellValues array
+    			if(possibleValue <= grid.getCell(xy[0], xy[1]))
+    			{
+    				//Step backwards if end of array is reached
+    				if(index == possibleCellValues.length - 1)
+    				{
+    					backtrack = true;
+    					grid.resetCell(xy[0], xy[1]);
+    					this.stepBackward();
+    					continue outerloop;
+    				}
+    				else
+    				{
+    					index++;
+    					continue;
+    				}
+    			}
+    			
+    			//Set cell value to next possible value
+    			grid.setCell(this.xy[0], this.xy[1], possibleValue);
+    			
+    			//If value already exists in row
+    			if(grid.rowContains(this.xy[1], grid.getCell(this.xy[0], this.xy[1])) > 1)
+    			{
+    				//Step backwards if end of array is reached
+    				if(index == possibleCellValues.length - 1)
+    				{
+    					backtrack = true;
+    					grid.resetCell(xy[0], xy[1]);
+    					this.stepBackward();
+    					continue outerloop;
+    				}
+    				else
+    				{
+    					index++;
+    					continue;
+    				}
+    			}
+    			
+    			//If value already exists in column
+    			if(grid.colContains(this.xy[0], grid.getCell(this.xy[0], this.xy[1])) > 1)
+    			{
+    				//Step backwards if end of array is reached
+    				if(index == possibleCellValues.length - 1)
+    				{
+    					backtrack = true;
+    					grid.resetCell(xy[0], xy[1]);
+    					this.stepBackward();
+    					continue outerloop;
+    				}
+    				else
+    				{
+    					index++;
+    					continue;
+    				}
+    			}
+    			
+    			//If value already exists in quadrant
+    			if(grid.quadrantContains(grid.getQuadrant(this.xy[0], this.xy[1]), grid.getCell(this.xy[0], this.xy[1])) > 1)
+    			{
+    				//Step backwards if end of array is reached
+    				if(index == possibleCellValues.length - 1)
+    				{
+    					backtrack = true;
+    					grid.resetCell(xy[0], xy[1]);
+    					this.stepBackward();
+    					continue outerloop;
+    				}
+    				else
+    				{
+    					index++;
+    					continue;
+    				}
+    			}
+    				
+    			break;
+    		}
+    		
+    		//If reached, valid cell value found
+    		this.stepForward();
+    		
+    		//If solution found, return
+    		if(grid.isValid())
+    		{
+    			break;
+    		}
+    		
+    		continue;
+    	}
+
+    	this.solvedGrid = grid.getGrid();
+    	return this.solvedGrid;
+    }
+    
+    /** Move the grid position handle to the next cell.
+      */
+    private void stepForward()
+    {
+    	if(this.xy[0] >= 8 && this.xy[1] >= 8)
+    		return;
+    	
+    	if(this.xy[0] == 8)
+    	{
+    		this.xy[0] = 0;
+    		this.xy[1]++;
     	}
     	else
     	{
-    		xy[0]++;
+    		this.xy[0]++;
     	}
-    	
-    	return xy;
     }
     
-    private int[] stepBackward(int[] xy)
+    /** Move the grid position handle to the previous cell.
+      */
+    private void stepBackward()
     {
-    	if(xy[0] <= 8 && xy[1] <= 0)
-    		return xy;
+    	if(xy[0] <= 0 && xy[1] <= 0)
+    		return;
     	
     	if(xy[0] == 0)
     	{
@@ -88,41 +241,47 @@ public class SudokuSolver
     	}
     	else
     	{
-    		xy[0]++;
+    		xy[0]--;
     	}
-    	
-    	return xy;
     }
     
-    public static void print(int[][] grid)
+    /** Get the column position of the grid position handle.
+      * @return the column position of the position handle
+      */
+    public int getXPos()
     {
-    	System.out.println("+===+===+===+===+===+===+===+===+===+");
-    	for(int i = 0; i < grid.length; i++)
-    	{
-    		System.out.print("|");
-    		for(int o = 0; o < grid[0].length; o++)
-    		{
-    			System.out.print(" " + grid[i][o] + " ");
-    			
-    			if((o+1) % 3 == 0)
-    			{
-    				System.out.print("|");
-    			}
-    			else
-    			{
-    				System.out.print("¦");
-    			}
-    		}
-    		System.out.println();
-    		
-    		if((i+1) % 3 == 0)
-    		{
-    			System.out.println("+===+===+===+===+===+===+===+===+===+");
-    		}
-    		else
-    		{
-    			System.out.println("+---+---+---+---+---+---+---+---+---+");
-    		}
-    	}
+    	return this.xy[0];
+    }
+    
+    /** Get the column position of the grid position handle.
+     * @return the row position of the position handle
+     */
+    public int getYPos()
+    {
+    	return this.xy[1];
+    }
+    
+    /** Get the number of iterations needed to solve the puzzle.
+      * @return the number of iterations needed to solve the puzzle
+      */
+    public int getIterations()
+    {
+    	return this.iterations;
+    }
+    
+    /** Get the solved sudoku grid.
+      * @return the solved sudoku grid
+      */
+    public int[][] getSolvedGrid()
+    {
+    	return this.solvedGrid;
+    }
+    
+    /** Get the initial sudoku grid.
+      * @return the initial sudoku grid
+     */
+    public int[][] getInitialGrid()
+    {
+    	return this.initialGrid;
     }
 }
