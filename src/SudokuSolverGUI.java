064574bb3fadf326f9a1c70366ddb116ae706a38
Brandon Richardson <brandon.richardson@introhive.com>
2017-02-12 15:46:01 -0400

    Improved performance metrics

--- src/SudokuSolverGUI.java
+++ src/SudokuSolverGUI.java
@@ -2,7 +2,8 @@ public class SudokuSolverGUI
 {
 	public static void main(String[] args)
     {
-    	int[][] initGrid = new int[][]
+    	//Build Sudoku Puzzle
+		int[][] initGrid = new int[][]
     	{
     		{8,0,0,0,0,0,0,0,0},
     		{0,0,3,6,0,0,0,0,0},
@@ -15,21 +16,26 @@ public class SudokuSolverGUI
     		{0,9,0,0,0,0,4,0,0}
     	};
     	
+    	//Initialize Solver and Print
     	SudokuSolver solver = new SudokuSolver(initGrid);
-    	
     	SudokuSolverGUI.print(solver.getInitialGrid());
+    	
+    	//Run Solver
     	String timeStarted = new java.text.SimpleDateFormat("HH:mm:ss.SSS").format(java.util.Calendar.getInstance().getTime());
-    	long timeStart = java.util.Calendar.getInstance().get(java.util.Calendar.MILLISECOND);
-    	
+    	long timeStart = System.nanoTime();
     	solver.solve();
-    	
     	String timeEnded = new java.text.SimpleDateFormat("HH:mm:ss.SSS").format(java.util.Calendar.getInstance().getTime());
-    	long timeEnd = java.util.Calendar.getInstance().get(java.util.Calendar.MILLISECOND);
+    	long timeEnd = System.nanoTime();
+    	double timeElapsed = timeEnd - timeStart;
+    	timeElapsed /= 1000000;
+    	
+    	//Print Solution
     	SudokuSolverGUI.print(solver.getSolvedGrid());
     	
+    	//Output Metrics
     	System.out.println("Time Started: " + timeStarted);
     	System.out.println("Time Finished: " + timeEnded);
-    	System.out.println("Time Elapsed: " + (timeEnd - timeStart) + "ms");
+    	System.out.println("Time Elapsed: " + timeElapsed + "ms");
     	System.out.println("Iterations: " + solver.getIterations());
     }
 	
